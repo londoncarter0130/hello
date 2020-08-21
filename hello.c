a7c2eb24336e8ddb61716ec05ecdfd0857b1f36e
London Carter <lcarter5@huskers.unl.edugit config --global user.email lcarter5@huskers.unl.edulscdgit config --global user.name London>
2020-08-21 07:28:24 +0000

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author:  London Carter
+ * Date:    2020
+ *
+ * Hack1.0
+ */
+  #include <stdlib.h>
+ #include <stdio.h>
+
+int main(int argc, char **argv) {
+
+printf ("London Carter!");
+printf ("Major: Computer Sciences");
+
+}
